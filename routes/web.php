e33c43c7110a27d5663e30615a761b867ae4553e
KarolineRRocha <emaildakarolineribeiro@gmail.com>
2024-11-21 20:39:58 +0000

    Comentando rotas home na web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 // Redirecionar '/' para '/home'
-Route::get('/', [HomeController::class, 'index'])->name('home');
+/* Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home'); */
 
 //Rota de fallback (Tratamento de exceção do 404)
 /* Route::fallback(function () {
